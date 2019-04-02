6350feb040b69a9f6c0161eb935f156cf0a5b1dd
Alpenglow88 <igoddard77@gmail.com>
2019-04-02 22:35:00 +0100

    fixing precompile asset failing issue

--- config/application.rb
+++ config/application.rb
@@ -2,14 +2,13 @@ require_relative 'boot'
 
 require 'rails/all'
 
-  config.assets.initialize_on_precompile = false
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module MovieDatabase
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
